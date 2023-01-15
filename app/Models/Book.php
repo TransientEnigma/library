0312113b6188a0b15cca28e85282be99e21ac9d8
TransientEnigma <e-engineer@hotmail.co.uk>
2023-01-15 21:52:37 +0000

    Add new path to books resource

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $guarded = [];
+
+    public function path()
+    {
+        // will look something like /books/1-enders-game
+        return '/books/' . $this->id;
+    }
 }
