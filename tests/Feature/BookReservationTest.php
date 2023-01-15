4983d20d7371cfbae64695e7f3674cda655ec7cf
TransientEnigma <e-engineer@hotmail.co.uk>
2023-01-15 01:27:12 +0000

    created Book Reservation Tests
    Signed-off-by: TransientEnigma <e-engineer@hotmail.co.uk>

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -2,20 +2,72 @@
 
 namespace Tests\Feature;
 
+use App\Models\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BookReservationTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+
+       $response = $this->post('/books', [
+           'title' => 'Cool Book Title',
+           'author' => 'Victor',
+       ]);
+
+       $response->assertOk();
+
+       $this->assertCount(1, Book::all());
+    }
+    /** @test */
+    public function a_title_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        // create a book in the database
+        $this->post('/books', [
+            'title' => 'Old Title',
+            'author' => 'Old Author',
+        ]);
+
+        // we need the id of book to update
+        $book = Book::first();
+
+
+        // update the book
+        $this->patch('/books/' . $book->id, [
+            'title' => 'New Title',
+            'author' => 'New Author',
+        ]);
+
+        $this->assertEquals('New Title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
     }
 }
